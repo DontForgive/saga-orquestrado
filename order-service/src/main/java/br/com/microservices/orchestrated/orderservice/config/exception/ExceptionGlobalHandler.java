d7b85828f8fd4f88c5d5ac7e4081240b2204b070
Gabriel Arruda <act.gabriel@agi.com.br>
2025-08-27 14:23:09 -0300

    Criado Exceptions

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/config/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,17 @@
+package br.com.microservices.orchestrated.orderservice.config.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionGlobalHandler {
+
+    @ExceptionHandler(ValidationsException.class)
+    public ResponseEntity<?> handleValidationException(ValidationsException validationsException){
+        var details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(),validationsException.getMessage());
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+    }
+}
